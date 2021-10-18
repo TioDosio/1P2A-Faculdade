93ebd34809e586f5e9e4dcf68e6724452c58cffe
Andre <andre.teodosio@tecnico.ulisboa.pt>
2021-10-18 22:38:40 +0100

    aaaaaaaaaaaaaaaaaaaaaaa

--- AED/Lab/Lab4/src/ex1.c
+++ AED/Lab/Lab4/src/ex1.c
@@ -1,1 +1,4 @@
 T(N) = T(10) + 2(T(N/2))
+/* A função em causa é recursiva e por essa razão chama-se a ela própria duas vezez, 
+a primeira para metade do problema e em seguida a segunda medate,
+então, a complexidade total vai depender da complexidade das sucessivas metades da função. */
